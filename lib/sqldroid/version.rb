134fdbae67fb090f2d95b602f268c2f13dbf8bab
Uwe Kubosch <uwe@datek.no>
2019-09-09 12:59:32 +0200

    Use beta version to test release process

--- lib/sqldroid/version.rb
+++ lib/sqldroid/version.rb
@@ -1,5 +1,5 @@
 module SQLDroid
   pom = File.read(File.expand_path('../../pom.xml', File.dirname(__FILE__)))
   MAVEN_VERSION = pom[%r{(?<=<version>)([0-9a-zA-Z.-]*)(?=</version>)}]
-  VERSION = MAVEN_VERSION.gsub('-SNAPSHOT', '.alpha.0')
+  VERSION = Gem::Version.new(MAVEN_VERSION.gsub('-SNAPSHOT', '')).send(:_segments).join('.')
 end
