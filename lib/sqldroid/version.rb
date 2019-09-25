61c014b4c4f4c34f92d0e7565572a989fc80684a
Uwe Kubosch <uwe@datek.no>
2019-09-25 11:19:15 +0200

    Try reading the pom from META-INF

--- lib/sqldroid/version.rb
+++ lib/sqldroid/version.rb
@@ -1,5 +1,8 @@
 module SQLDroid
-  pom = File.read(File.expand_path('../../pom.xml', File.dirname(__FILE__)))
-  MAVEN_VERSION = pom[%r{(?<=<version>)([0-9a-zA-Z.-]*)(?=</version>)}]
+  pom_locations = [
+      File.expand_path('../META-INF/org.sqldroid/sqldroid/pom.xml', __dir__),
+      File.expand_path('../../pom.xml', __dir__),
+  ]
+  MAVEN_VERSION = File.read(pom_locations.find { |f| File.exist? f })[%r{(?<=<version>)([0-9a-zA-Z.-]*)(?=</version>)}]
   VERSION = Gem::Version.new(MAVEN_VERSION.gsub('-SNAPSHOT', '')).send(:_segments).join('.')
 end
